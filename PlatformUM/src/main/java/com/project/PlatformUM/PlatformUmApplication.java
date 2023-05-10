18e3dccef4c65dfcdd3e72db2bac210346ad49b6
Nicolas Mayoral <n.mayoral@alumno.um.edu.ar>
2023-05-10 19:27:27 -0300

    Close #2 Project structure successfully created

---
+++ PlatformUM/src/main/java/com/project/PlatformUM/PlatformUmApplication.java
@@ -0,0 +1,13 @@
+package com.project.PlatformUM;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlatformUmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlatformUmApplication.class, args);
+	}
+
+}
