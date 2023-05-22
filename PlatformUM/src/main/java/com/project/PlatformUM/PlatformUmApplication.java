306f1200cbb729a909d6d1ed364a169f85021632
Nicolas Mayoral <n.mayoral@alumno.um.edu.ar>
2023-05-21 23:03:10 -0300

    #6 Hibernate configuration Started need to fix dependency, env variables implemented

--- PlatformUM/src/main/java/com/project/PlatformUM/PlatformUmApplication.java
+++ PlatformUM/src/main/java/com/project/PlatformUM/PlatformUmApplication.java
@@ -2,11 +2,27 @@ package com.project.PlatformUM;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import io.github.cdimascio.dotenv.Dotenv;
 
 @SpringBootApplication
 public class PlatformUmApplication {
 
 	public static void main(String[] args) {
+		Dotenv dotenv = Dotenv.configure().load();
+
+        // Get the values from .env file
+        String dbUrl = dotenv.get("DB_URL");
+        String dbUsername = dotenv.get("DB_USERNAME");
+        String dbPassword = dotenv.get("DB_PASSWORD");
+        String dbDriverClassName = dotenv.get("DB_DRIVER_CLASS_NAME");
+
+        // Configure the database settings as system properties
+        System.setProperty("DB_URL", dbUrl);
+        System.setProperty("DB_USERNAME", dbUsername);
+        System.setProperty("DB_PASSWORD", dbPassword);
+        System.setProperty("DB_DRIVER_CLASS_NAME", dbDriverClassName);
+
+        // Spring boot application starter
 		SpringApplication.run(PlatformUmApplication.class, args);
 	}
 
