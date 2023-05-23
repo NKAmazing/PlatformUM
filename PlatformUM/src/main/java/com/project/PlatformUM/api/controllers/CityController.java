a0cabd52fea5f4efb8a90f56597886380478af21
Markitos <48894558+XxRaXoRxX@users.noreply.github.com>
2023-05-23 16:23:01 -0300

    #7 Created controllers

--- PlatformUM/src/main/java/com/project/PlatformUM/api/controllers/CityController.java
+++ PlatformUM/src/main/java/com/project/PlatformUM/api/controllers/CityController.java
@@ -1,4 +1,38 @@
 package com.project.PlatformUM.api.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+import com.project.PlatformUM.api.services.CityService;
+
+@RestController
+@RequestMapping("/api/cities")
 public class CityController {
+
+    @Autowired
+    private CityService cityService;
+
+    /*@GetMapping
+    public List<City> getCities() {
+        return cityService.getCities();
+    }
+
+    @GetMapping("/{id}")
+    public City getCity(@PathVariable("id") Long id) {
+        return cityService.getCity(id);
+    }
+
+    @PostMapping
+    public City addCity(@RequestBody City city) {
+        return cityService.addCity(city);
+    }
+
+    @PutMapping("/{id}")
+    public City updateCity(@PathVariable("id") Long id, @RequestBody City city) {
+        return cityService.updateCity(id, city);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteCity(@PathVariable("id") Long id) {
+        cityService.deleteCity(id);
+    }*/
 }
