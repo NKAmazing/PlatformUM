8a0bd227013be9bf62271cd65ef8e5e53850f96c
Nicolas Mayoral <n.mayoral@alumno.um.edu.ar>
2023-05-31 18:48:28 -0300

    Company - Vehicle relationship fixed

--- PlatformUM/src/main/java/com/project/PlatformUM/api/models/Company.java
+++ PlatformUM/src/main/java/com/project/PlatformUM/api/models/Company.java
@@ -2,7 +2,6 @@ package com.project.PlatformUM.api.models;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
-import jakarta.persistence.OneToOne;
 import jakarta.persistence.OneToMany;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -21,13 +20,8 @@ public class Company {
     @Column(nullable = false)
     private String name;
 
-    @Column(nullable = false)
-
     @OneToMany(mappedBy = "company", cascade = CascadeType.ALL)
-    private Set<Vehicle> vehicle = new HashSet<>();
-
-    @OneToOne(mappedBy = "company", cascade = CascadeType.ALL)
-    private Trip trip;    
+    private Set<Vehicle> vehicles = new HashSet<>();   
 
     public Company() {
         // Empty constructor
@@ -35,7 +29,7 @@ public class Company {
 
     public Company(String name, Set<Vehicle> vehicle, Trip trip) {
         this.name = name;
-        this.vehicle = vehicle;
+        this.vehicles = vehicle;
     }
 
     // Getters and setters of the class attributes
@@ -56,18 +50,10 @@ public class Company {
     }
 
     public Set<Vehicle> getVehicle() {
-        return vehicle;
+        return vehicles;
     }
 
     public void setVehicle(Set<Vehicle> vehicle) {
-        this.vehicle = vehicle;
-    }
-
-    public Trip getTrip() {
-        return trip;
-    }
-
-    public void setTrip(Trip trip) {
-        this.trip = trip;
+        this.vehicles = vehicle;
     }
 }
