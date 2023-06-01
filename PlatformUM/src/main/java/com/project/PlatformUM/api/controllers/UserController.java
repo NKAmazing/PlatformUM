ca9197f44a128b4a8a2671a293d89255897e9e84
Nicolas Mayoral <n.mayoral@alumno.um.edu.ar>
2023-05-31 21:00:21 -0300

    #7 makeReservation method Started

--- PlatformUM/src/main/java/com/project/PlatformUM/api/controllers/UserController.java
+++ PlatformUM/src/main/java/com/project/PlatformUM/api/controllers/UserController.java
@@ -3,10 +3,15 @@ package com.project.PlatformUM.api.controllers;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 import com.project.PlatformUM.api.services.UserService;
+// import com.project.PlatformUM.api.models.Reservation;
 import com.project.PlatformUM.api.models.User;
+// import com.project.PlatformUM.api.models.Trip;
 
 import java.util.List;
 import java.util.Optional;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
 
 @RestController
 @RequestMapping("/api/users")
@@ -14,6 +19,15 @@ public class UserController {
 
     @Autowired
     private UserService userService;
+
+    // private final ReservationController reservationController;
+
+    // private final TripController tripController;
+
+    // public UserController(ReservationController reservationController, TripController tripController) {
+    //     this.reservationController = reservationController;
+    //     this.tripController = tripController;
+    // }
 
     @GetMapping
     public List<User> getUsers() {
@@ -45,4 +59,23 @@ public class UserController {
             return "City with id " + id + " was not deleted.";
         }
     }
+
+    // public List<Trip> searchTrips() {
+    //     // Call the method to search the trips
+    //     return tripController.getTrips();
+    // };
+
+    // @PostMapping("/{userId}/makeReservation")
+    // public Reservation makeReservation(@PathVariable("userId") Long userId, @RequestBody Trip trip) {
+    //     // Get the user
+    //     Optional<User> user = getUser(userId);
+    
+    //     // Create a new reservation
+    //     Reservation reservation = new Reservation();
+    //     reservation.setUser(user.get());
+    //     reservation.setTrip(trip);        
+
+    //     // Call the method to create the reservation
+    //     return reservationController.addReservation(reservation);
+    // }    
 }
